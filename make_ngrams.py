ca5c45297f315c5c0090e6e471a71725ea32e657
Trent Robbins <robbintt@gmail.com>
2017-09-02 14:38:48 -0700

    Finished the ngram maker.

--- make_ngrams.py
+++ make_ngrams.py
@@ -28,7 +28,14 @@ def clean_the_dictionary(contents):
         word = word.strip().lower()
         for letter in word:
             if letter not in letters:
+                print word
                 continue
+
+        # remove the word if the word is too short to contain length 2+ n-grams
+        if len(word) < 2:
+            print word
+            continue
+
         dictionary.append(word)
 
     # de-duplicate and convert back to a list
@@ -45,6 +52,43 @@ def get_average_word_length(dictionary):
 
     # average the dictionary
     return float(total) / len(dictionary)
+
+
+def get_some_ngrams(size, word):
+    ''' get all the <size>-grams for <word>
+    word => string
+    size => int
+
+    returns a list of ngrams
+    '''
+    some_ngrams = list()
+    
+    # the max range integer we should pass is len(word)+1
+    for i in range(len(word)-size+1):
+        some_ngrams.append(word[i:i+size])
+
+    return some_ngrams
+
+
+def make_ngrams(dictionary):
+    ''' make the dict of ngrams from a word dictionary
+    dictionary => list()
+
+    return a list of strings
+    '''
+    ngrams = list()
+    count = 0
+    for word in dictionary:
+        # get all the ngrams for the word
+        for size in range(2,len(word)):
+            ngrams.extend(get_some_ngrams(size, word))
+
+        # a temporary clause to count word progress
+        count += 1
+        if count % 100:
+            print "{} words or {}% of ~235886".format(count, float(count)/235886)
+
+    return ngrams
 
 
 def add_ngrams(input_string):
@@ -66,18 +110,31 @@ if __name__ == '__main__':
     average_word_length = get_average_word_length(dictionary)
 
     # need to build dict of ngrams for sqlite storage
+    ngrams = make_ngrams(dictionary)
+
+    # assemble the frequency dict from the expanded ngram list
+    # this list is estimated to be 65 megabytes (the dict should be smaller by some amount)
+    print("Assembling the ngram dictionary...")
+    ngram_freq_dict = dict()
+    for ngram in ngrams:
+        ngram_freq_dict[ngram] = ngram_freq_dict.get(ngram, 0) + 1
 
     conn = sqlite3.connect(NGRAM_DATABASE)
     c = conn.cursor()
 
     # table should take (ngram, freq)
     table = 'ngrams'
-    INSERT_TEMPLATE = 'INSERT INTO  {table} VALUES (?,?)'
+    INSERT_TEMPLATE = 'INSERT INTO {table} VALUES (?,?)'
     INSERT_TEMPLATE = INSERT_TEMPLATE.format(table=table)
 
-    c.executemany(INSERT_TEMPLATE, ngrams)
+    print("Inserting ngrams into ngrams sqlite3 table.")
+    for ngram, freq in ngram_freq_dict.items():
+        # check it out!!
+        if freq > 10:
+            print ngram, freq
+        c.execute(INSERT_TEMPLATE, (ngram, freq))
 
-
-
-
+    print("Commit the database entries (many MBs)")
+    conn.commit()
+    conn.close()
 
